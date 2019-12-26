a08e0bbd1dcc8815ee702020f0c1be871f18da52
ornaishtat <naishtat.or@gmail.com>
2019-12-26 08:52:03 +0000

    first commit

---
+++ artifactoryinstaller.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+###################################################
+# Author: NewLight- Or Naishtat
+# Date: 26.12.2019
+# Version: 0.1
+# Purpose: This script will install artifactory on RPM or DEB systems from a ZIP file.
+# Objectives:
+# 1. Check all pre-conditions are met for Artifactory.
+# 2. Check which system the user is using and download+install the correct package apropriately.
+# 3. Install Artifactory.
+# 4. Run tests to make sure Artifactory is installed 
+###################################################
+
+## Variables ##
+deco_time=2
+deco_line='####################################################'
+###############
+
